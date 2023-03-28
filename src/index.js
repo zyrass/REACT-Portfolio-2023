e9196025fd795ed5afb241a1f0ff6878a9c741cb
admin <admin@CRN-S20-iMac-ITK-10.local>
2023-03-28 17:03:07 +0200

    mise à jour 2023

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Dependancies
 import React from "react";
-import { createRoot } from 'react-dom';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -17,16 +17,14 @@ import { themeReducer } from './Store/Reducers/themeReducers';
 
 const store = createStore( themeReducer )
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <Router>
-          <Provider store={ store }>
-              <App />
-          </Provider>
-      </Router>
-  </React.StrictMode>
-);
+const root = ReactDOM.render(<React.StrictMode>
+    <Router>
+        <Provider store={ store }>
+            <App />
+        </Provider>
+    </Router>
+</React.StrictMode>, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
